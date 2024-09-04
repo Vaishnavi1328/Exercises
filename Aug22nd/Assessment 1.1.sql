9b10acee240bac88943d31b35370c49377bb553c
Vaishnavi <113687715+Vaishnavi1328@users.noreply.github.com>
2024-09-04 10:26:26 +0530

    commit

---
+++ Aug22nd/Assessment 1.1.sql
@@ -0,0 +1,81 @@
+--1. ""Joins exercise""
+--Calculate the total amount spent by each customer.
+SELECT o.customer_id,
+SUM(p.price * o.quantity) AS total_spent
+FROM Orders o
+JOIN Products p ON o.product_id = p.product_id
+GROUP BY o.customer_id;
+
+--Find customers who have spent more than $1,000 in total.
+SELECT o.customer_id,
+SUM(p.price * o.quantity) AS total_spent
+FROM Orders o
+JOIN Products p ON o.product_id = p.product_id
+GROUP BY o.customer_id
+HAVING SUM(p.price * o.quantity) > 1000;
+
+---Find Product Categories with more Than 5 Products
+SELECT category,
+COUNT(product_id) AS product_count
+FROM Products
+GROUP BY category
+HAVING COUNT(product_id) > 5;
+
+--Calculate the total number of products for each category and supplier combination.
+SELECT category,supplier,
+COUNT(product_id) AS total_products
+FROM Products
+GROUP BY category, supplier
+ORDER BY category, supplier;
+
+
+--Summarize total sales by product and customer, and also provide an overall total.
+
+-- Total Sales by Product and Customer
+SELECT o.customer_id,p.product_id,p.product_name,
+SUM(o.quantity * p.price) AS total_sales
+FROM Orders o
+JOIN Products p ON o.product_id = p.product_id
+GROUP BY o.customer_id, p.product_id, p.product_name
+ORDER BY o.customer_id, p.product_id;
+
+-- Overall Total Sales
+SELECT SUM(o.quantity * p.price) AS overall_total_sales
+FROM Orders o
+JOIN Products p ON o.product_id = p.product_id;
+
+--2. ""Stored Procedure""
+--Insert operation
+CREATE PROCEDURE InsertProduct
+    @product_id INT,
+    @product_name NVARCHAR(255),
+    @price DECIMAL(10, 2),
+    @category NVARCHAR(255),
+    @supplier NVARCHAR(255)
+AS
+BEGIN
+    INSERT INTO Products (product_id, product_name, price, category, supplier)
+    VALUES (@product_id, @product_name, @price, @category, @supplier);
+END;
+
+--update operation
+CREATE PROCEDURE UpdateProduct
+    @product_id INT,
+    @price DECIMAL(10, 2),
+    @category NVARCHAR(255)
+AS
+BEGIN
+    UPDATE Products
+    SET price = @price,
+        category = @category
+    WHERE product_id = @product_id;
+END;
+
+--delete operation
+CREATE PROCEDURE DeleteProduct
+    @product_id INT
+AS
+BEGIN
+    DELETE FROM Products
+    WHERE product_id = @product_id;
+END;
---
+++ Aug22nd/Assessment 1.2.sql
@@ -0,0 +1,158 @@
+--Afternoon assesment
+--1
+SELECT *
+FROM Products
+WHERE Category = 'Electronics'
+  AND Price > 500;
+
+--2
+SELECT SUM(Quantity) AS TotalQuantitySold
+FROM Orders;
+
+--3
+SELECT 
+    p.ProductName,
+    SUM(o.Quantity * p.Price) AS TotalRevenue
+FROM 
+    Orders o
+JOIN 
+    Products p ON o.ProductID = p.ProductID
+GROUP BY 
+    p.ProductName;
+
+--4
+SELECT 
+    p.ProductName,
+    SUM(o.Quantity * p.Price) AS TotalRevenue
+FROM 
+    Orders o
+JOIN 
+    Products p ON o.ProductID = p.ProductID
+GROUP BY 
+    p.ProductName
+HAVING 
+    SUM(o.Quantity * p.Price) > 30000
+ORDER BY 
+    TotalRevenue DESC;
+--Order of Execution:where->groupby->having->orderby
+
+--5
+SELECT 
+    Category,
+    COUNT(ProductName) AS NoOfProducts
+FROM 
+    Products
+GROUP BY 
+    Category;
+
+--6
+SELECT 
+    c.CustomerID,
+    c.FirstName
+FROM 
+    Orders o
+JOIN 
+    Customers c ON o.CustomerID = c.CustomerID
+GROUP BY 
+    c.CustomerID,
+    c.FirstName
+HAVING 
+    COUNT(o.OrderID) > 5;
+
+--Stored procedure exercise
+--1
+CREATE PROCEDURE GetAllCustomers
+AS
+BEGIN
+    SELECT * FROM Customers;
+END;
+
+EXEC GetAllCustomers;
+
+--2
+CREATE PROCEDURE GetOrderDetailsByOrderId
+	@OrderId INT
+AS
+BEGIN
+	SELECT * FROM Orders WHERE OrderId=@OrderId;
+END;
+
+EXEC GetOrderDetailsByOrderId @OrderId=3;
+
+--3
+CREATE PROCEDURE GetProductsByCategoryAndPrice
+    @Category VARCHAR(50),
+    @MinPrice DECIMAL(10, 2)
+AS
+BEGIN
+    SELECT *
+    FROM Products
+    WHERE Category = @Category
+      AND Price >= @MinPrice;
+END;
+
+EXEC GetProductsByCategoryAndPrice @Category='Electronics',@MinPrice=7000.00;
+
+--4
+CREATE PROCEDURE InsertNewProduct
+    @ProductName VARCHAR(100),
+    @Category VARCHAR(50),
+    @Price DECIMAL(10, 2),
+    @StockQuantity INT
+AS
+BEGIN
+    INSERT INTO Products (ProductName, Category, Price, StockQuantity)
+    VALUES (@ProductName, @Category, @Price, @StockQuantity);
+END
+
+EXEC InsertNewProduct 
+    @ProductName = 'New Product', 
+    @Category = 'Electronics', 
+    @Price = 499.99, 
+    @StockQuantity = 100;
+
+--5
+CREATE PROCEDURE UpdateCustomerEmail
+    @CustomerID INT,
+    @NewEmail VARCHAR(100)
+AS
+BEGIN
+    UPDATE Customers
+    SET Email = @NewEmail
+    WHERE CustomerID = @CustomerID;
+END;
+
+EXEC UpdateCustomerEmail 
+    @CustomerID = 3, 
+    @NewEmail = 'new.email@example.com';
+
+--6
+CREATE PROCEDURE DeleteOrderByID
+    @OrderID INT
+AS
+BEGIN
+    DELETE FROM Orders
+    WHERE OrderID = @OrderID;
+END;
+
+EXEC DeleteOrderByID @OrderID = 3;
+
+--7
+CREATE PROCEDURE GetTotalProductsInCategory
+    @Category VARCHAR(50),
+    @TotalProducts INT OUTPUT
+AS
+BEGIN
+    SET @TotalProducts = 0;
+    SELECT @TotalProducts = COUNT(*)
+    FROM Products
+    WHERE Category = @Category;
+END;
+
+DECLARE @ProductCount INT;
+
+EXEC GetTotalProductsInCategory 
+    @Category = 'Electronics', 
+    @TotalProducts = @ProductCount OUTPUT;
+
+SELECT @ProductCount AS TotalProducts;
